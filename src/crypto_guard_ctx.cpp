f6694d61638ba4f31f11a657625c7a84f0700556
Aiwe1 <i-like-big-boobs@yandex.ru>
2025-10-14 21:54:38 +0000

    modified:   include/crypto_guard_ctx.h 	modified:   src/crypto_guard_ctx.cpp

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,115 @@
+#include <openssl/evp.h>
+#include <array>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "crypto_guard_ctx.h"
+
 
 namespace CryptoGuard {
+
+struct AesCipherParams {
+    static const size_t KEY_SIZE = 32;             // AES-256 key size
+    static const size_t IV_SIZE = 16;              // AES block size (IV length)
+    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
+
+    int encrypt;                              // 1 for encryption, 0 for decryption
+    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
+    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
+};
+
+class CryptoGuardCtx::Impl {
+public:
+    AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
+        AesCipherParams params;
+        
+        constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+        int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
+                                    reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
+                                    params.key.data(), params.iv.data());
+
+        if (result == 0) {
+            throw std::runtime_error{"Failed to create a key from password"};
+        }
+
+        return params;
+    }
+
+    Impl() {
+        OpenSSL_add_all_algorithms();
+        ctx = EVP_CIPHER_CTX_new();
+        if (!ctx) {
+            throw std::runtime_error{"EVP new error."};
+        }
+    }
+
+    ~Impl() {
+        EVP_CIPHER_CTX_free(ctx);
+        //std::print("String encoded successfully. Result: '{}'\n\n", output);
+        EVP_cleanup();
+    }
+
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+        params = CreateChiperParamsFromPassword(password);
+        params.encrypt = 1;
+
+        if (!EVP_CipherInit_ex(ctx, params.cipher, nullptr, params.key.data(), params.iv.data(), params.encrypt)) {
+            throw std::runtime_error{"error encrypt."};
+        }
+
+        std::vector<unsigned char> outBuf(N + EVP_MAX_BLOCK_LENGTH);
+        std::vector<unsigned char> inBuf;
+        inBuf.reserve(N);
+        int outLen;
+
+        for(; !inStream.eof();) {
+            inBuf.clear();
+            unsigned char c;
+            for (int i = 0; i < N && inStream >> c;  ++i) {
+                inBuf.push_back(c);
+            }
+
+            EVP_CipherUpdate(ctx, outBuf.data(), &outLen, inBuf.data(), static_cast<int>(inBuf.size()));
+            for (int i = 0; i < outLen; ++i)
+                outStream << outBuf[i];
+        }
+        
+        // Заканчиваем работу с cipher
+        EVP_CipherFinal_ex(ctx, outBuf.data(), &outLen);
+        for (int i = 0; i < outLen; ++i)
+              outStream << outBuf[i];
+    }
+
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+
+
+    }
+
+    std::string CalculateChecksum(std::iostream &inStream) {
+        return "NOT_IMPLEMENTED"; 
+    }
+private:
+    const int N = 1024;
+    AesCipherParams params;
+    EVP_CIPHER_CTX* ctx;
+};
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>()) {}
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+CryptoGuardCtx::CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
+CryptoGuardCtx & CryptoGuardCtx::operator=(CryptoGuardCtx &&) noexcept = default;
+
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->EncryptFile(inStream, outStream, password);
+}
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->DecryptFile(inStream, outStream, password);
+}
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) {
+    return pImpl_->CalculateChecksum(inStream); 
+}
 
 }  // namespace CryptoGuard
